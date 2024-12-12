d03b2b609c877116f6972b7cc60e947e4a4fbcd0
Muhammad Shahwal <sawalrever23@gmail.com>
2024-12-12 21:42:15 +0700

    feat: add logo app and splash screen

---
+++ app/src/main/java/com/bangkit/capstone/planitorium/ui/welcome/SplashScreen.kt
@@ -0,0 +1,53 @@
+package com.bangkit.capstone.planitorium.ui.welcome
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.capstone.planitorium.databinding.ActivitySplashScreenBinding
+import com.bangkit.capstone.planitorium.ui.main.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMainActivity()
+        }, 3000)
+    }
+
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+}
